580a77fa0187ec5db0f5014fc74125edf5b56753
kvgamedev <kuberrv2005@gmail.com>
2025-09-14 20:05:54 +0530

    nvim: updated autocommands

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,26 +1,21 @@
-local autocmd = vim.api.nvim_create_autocmd
+local gr = vim.api.nvim_create_augroup("User", { clear = true })
+local au = function(event, pattern, once, callback)
+	vim.api.nvim_create_autocmd(event, {
+		group = gr,
+		pattern = pattern,
+		once = once,
+		callback = callback,
+	})
+end
 
-autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.on_yank()
-	end,
-})
-
-autocmd("VimResized", {
-	callback = function()
-		vim.cmd("tabdo wimcd =")
-		vim.cmd("tabnext" .. vim.fn.tabpagenr())
-	end,
-})
-
-autocmd("LspAttach", {
-	once = true,
-	callback = function()
-		vim.keymap.set("n", "gl", "<Nop>", { desc = "LSP" })
-		vim.keymap.set("n", "gld", vim.lsp.buf.definition, { desc = "Goto Definition" })
-		vim.keymap.set("n", "gli", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
-		vim.keymap.set("n", "glt", vim.lsp.buf.type_definition, { desc = "Goto Type Definition" })
-		vim.keymap.set("n", "gln", vim.lsp.buf.rename, { desc = "Rename" })
-		vim.keymap.set("n", "gla", vim.lsp.buf.code_action, { desc = "Code Actions" })
-	end,
-})
+au("TextYankPost", "*", false, function()
+	vim.hl.on_yank()
+end)
+au("LspAttach", "*", true, function()
+	vim.keymap.set("n", "gl", "<Nop>", { desc = "LSP" })
+	vim.keymap.set("n", "gld", vim.lsp.buf.definition, { desc = "Goto Definition" })
+	vim.keymap.set("n", "gli", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
+	vim.keymap.set("n", "glt", vim.lsp.buf.type_definition, { desc = "Goto Type Definition" })
+	vim.keymap.set("n", "gln", vim.lsp.buf.rename, { desc = "Rename" })
+	vim.keymap.set("n", "gla", vim.lsp.buf.code_action, { desc = "Code Actions" })
+end)
