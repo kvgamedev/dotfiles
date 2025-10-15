3fe5de2677cbc11bca61e3b303a5dc6f205f9d74
kvgamedev <kuberrv2005@gmail.com>
2025-10-15 23:50:30 +0530

    startup.lua

---
+++ nvim/lua/startup.lua
@@ -0,0 +1,12 @@
+local map = vim.keymap.set
+
+vim.pack.add({
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
+	"https://github.com/rose-pine/neovim",
+	"https://github.com/rebelot/kanagawa.nvim",
+	"https://github.com/folke/tokyonight.nvim",
+	"https://github.com/ellisonleao/gruvbox.nvim",
+	"https://github.com/navarasu/onedark.nvim",
+})
+
+vim.cmd("colorscheme rose-pine-moon")
